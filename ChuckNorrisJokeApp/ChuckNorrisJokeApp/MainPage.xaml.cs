bbe915fa9f2004c8168d644a969c08fcd01ed0ef
larencloete <56066363+larencloete@users.noreply.github.com>
2019-12-04 14:13:56 +0200

    My initial commit

---
+++ ChuckNorrisJokeApp/ChuckNorrisJokeApp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ChuckNorrisJokesLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ChuckNorrisJokeApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void JokeButton_Clicked(object sender, EventArgs e)
+        {
+            var instance = new JokeGenerator();
+            var joke = await instance.GetRandomJoke();
+            JokeLabel.Text = joke;
+        }
+        private async void CategoryButton_Clicked(object sender, EventArgs e)
+        {
+           await Navigation.PushAsync(new CategoryPage());
+        }
+
+
+
+    }   
+}
---
+++ ChuckNorrisJokeApp/ChuckNorrisJokesLibrary/JokeGenerator.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisJokesLibrary
+{
+    public class JokeGenerator
+    {
+        public async Task<string[]> GetCategories()
+        {
+            HttpClient client = new HttpClient();
+
+            string categoryString = await client.GetStringAsync("https://api.chucknorris.io/jokes/categories");
+
+            var categories = JsonConvert.DeserializeObject<string[]>(categoryString);
+
+
+            return categories;
+        }
+
+        public async Task<string> GetRandomJoke()
+        {
+            HttpClient client = new HttpClient();
+
+            string randomJokeString = await client.GetStringAsync("https://api.chucknorris.io/jokes/random");
+
+            var joke = JsonConvert.DeserializeObject<Joke>(randomJokeString);
+
+
+            return joke.value;
+
+        }
+
+         
+
+    }
+}
